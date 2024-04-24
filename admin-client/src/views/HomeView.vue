001528c9d053eb8446c51a44bb61017c356768e1
toantran292 <tranthaitoan998@gmail.com>
2024-04-24 19:19:00 +0700

    .

--- admin-client/src/views/HomeView.vue
+++ admin-client/src/views/HomeView.vue
@@ -18,6 +18,7 @@ import AppSlider from '@/components/layouts/AppSlider.vue'
 import { ref } from 'vue'
 import { useRoute } from 'vue-router'
 const route = useRoute()
-const selectedKeys = ref<string[]>(['users'])
+const selectedKeys = ref<string[]>([route.path.split('/')[1]])
 const collapsed = ref<boolean>(false)
+// console.log(route.path.split('/')[1])
 </script>
